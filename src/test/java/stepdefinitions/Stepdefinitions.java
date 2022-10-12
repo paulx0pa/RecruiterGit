17e290b076d2a53970f3365074ccfbeb8acf7715
paulx0pa <114465744+paulx0pa@users.noreply.github.com>
2022-10-12 11:52:05 +0530

    Update Stepdefinitions.java

--- src/test/java/stepdefinitions/Stepdefinitions.java
+++ src/test/java/stepdefinitions/Stepdefinitions.java
@@ -21,7 +21,7 @@ public class Stepdefinitions {
 		
 
 driver.get("https://google.com");
-		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/azureuser/chromedriver");
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
