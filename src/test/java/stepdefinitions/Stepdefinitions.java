1bb514cdc09293f45c35f2e80ad14a5ed08813cd
paulx0pa <114465744+paulx0pa@users.noreply.github.com>
2022-10-13 08:00:30 +0530

    Update Stepdefinitions.java

--- src/test/java/stepdefinitions/Stepdefinitions.java
+++ src/test/java/stepdefinitions/Stepdefinitions.java
@@ -23,9 +23,9 @@ public class Stepdefinitions {
 		options.addArguments("headless");
 		options.addArguments("disable-gpu");
 		driver=new ChromeDriver(options);
-		driver = new ChromeDriver();
-       driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
+       //driver = new ChromeDriver();
+       //driver.manage().deleteAllCookies();
+       // driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("http://staging.x0pa.ai/app/roboroy");  
         driver.findElement(By.id("hs-eu-confirmation-button")).click();
