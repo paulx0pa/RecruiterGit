a42bc92d564c1d4e81f7a136ca767166c9098c59
paulx0pa <114465744+paulx0pa@users.noreply.github.com>
2022-10-13 09:41:50 +0530

    Update Testrunners.java

--- src/test/java/testrunners/Testrunners.java
+++ src/test/java/testrunners/Testrunners.java
@@ -14,7 +14,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 		"featurefiles/SkillsandEdu.feature",
 		"featurefiles/additionalinfo.feature",
 		"featurefiles/prescreeningQ.feature"},glue="stepdefinitions",
-dryRun=false,plugin={"com.cucumber.listener.ExtentCucumberFormatter:Reports/reports.html"},tags="@RecruiterLogin,@RecruiterLogout,@CreateNewJob,@SkillsandEducation,@AdditionalInfo,@PreScreeningQuestions")
+dryRun=false,plugin={"com.cucumber.listener.ExtentCucumberFormatter:Reports/reports.html"},
+		 format={"pretty","html:target/cucumber","json:target/cucumber.json"},tags="@RecruiterLogin,@RecruiterLogout,@CreateNewJob,@SkillsandEducation,@AdditionalInfo,@PreScreeningQuestions")
 public class Testrunners extends AbstractTestNGCucumberTests
 {
 	
